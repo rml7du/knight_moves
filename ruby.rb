037992c50d323244d4762d494ea93e47361da6d7
Robert Lee <rml7du@gmail.com>
2020-07-09 11:36:31 -0600

    still have no idea what i am doign

--- ruby.rb
+++ ruby.rb
@@ -1,21 +1,37 @@
 
 class Node
-    attr_accessor :x, :y, :coordinants, :right, :left
-    def initialize(x,y)
-        @x = x
-        @y = y
+    attr_accessor :x, :y, :position, :coordinants, :left, :right
+    def initialize(x, y)
+        @position = [x,y]
         @left = nil
         @right = nil
+        @x = position[0]
+        @y = position[1]
         @coordinants = "[#{@x},#{@y}]"
     end
 end
 
+class Step
+    attr_accessor :position, :children, :parent
+
+    def initialize(position, children = nil, parent = nil)
+        @position = position
+        @children = []
+        @parent = parent
+    end
+end
+
+
 class Knight
     attr_accessor :board
 
+    @@moves = [ [-1,2], [1,2], [2,1], [2,-1], [1,-2], [-1,-2], [-2,-1], [-2,1] ]
+
     def initialize()
+        @available_positions =  []
         @board = build_board()
         @root = build_tree(@board)
+        @path = nil
     end
 
     def build_board()
@@ -24,8 +40,22 @@ class Knight
         8.times do |x|
             8.times { |y| board.push( Node.new(x+1, y+1) ) }
         end
-        board
+        board.each { |node| @available_positions.push(node.position) }
+        board 
     end
+
+    def build_paths(starting)
+        return nil if !valid_move(starting) || !@available_positions.include?(starting)
+        @path = Step.new(starting)
+        puts "path: #{@path}"
+        @available_positions.delete_if { |x| x == starting } #remove location from remaining options
+        @@moves.each do |move|
+            next_move = [starting[0]+move[0], starting[1]+move[1]]
+            @path.children.push(build_paths(next_move))
+        end
+        next_move
+    end 
+
 
     def build_tree(array)
         return if array.empty?
@@ -36,8 +66,8 @@ class Knight
         node
     end
 
-    def valid_move(x, y)
-        if x < 0 || y < 0 || x > 8 || y > 8
+    def valid_move(starting)
+        if starting[0] <= 0 || starting[1] <= 0 || starting[0] > 8 || starting[1] > 8
             return false
         end
         true
@@ -54,10 +84,17 @@ class Knight
         end
     end
 
-    def knight_moves()
-        moves = [[-1,2],[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1]]
+    def knight_moves(starting, ending)
+        
+        build_paths(starting)
+        puts "chillens #{@path.children}"
         #build_tree()
-        puts "You made it in 3 moves! Heres your path:"
+        #puts "You made it in 3 moves! Heres your path:"
+
+        #puts @available_positions.include?(starting)
+        #@available_positions.delete_if { |x| x == starting } #remove location from remaining options
+        #puts @available_positions.include?(starting)
+        #print @available_positions
     end 
 
     def inorder(node = @root)
@@ -71,9 +108,10 @@ class Knight
     end
 end
 
-knight = Knight.new()
-#puts knight.board.coordinants
-knight.board.each { |n| puts "#{n.coordinants}" }
+wyatt = Knight.new()
+#knight.board.each { |n| puts "#{n.coordinants}" }
+
+wyatt.knight_moves([1,1], [8,8])
 
 #knight_moves([3,3], [4,3])
 
